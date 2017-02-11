7b13d70749e97860d1c98c766b95cc2604171402
goujonpa <goujonpa@gmail.com>
2017-02-11 16:06:00 +0100

    comments recurrent routes script

--- heatmap.py
+++ heatmap.py
@@ -5,19 +5,43 @@ Generates an heatmap with every position recorded between two dates
 
 import numpy as np
 import matplotlib
-from sklearn import svm
-matplotlib.use('Agg')
+import datetime
+matplotlib.use('Agg')  # mandatory to be able to export plots from server
+# yet to be removed when working on your own machine
 import matplotlib.pyplot as plt
 import matplotlib.font_manager
 from mpl_toolkits.basemap import Basemap
+from sklearn import svm
 
-df = sqlContext.read.format(
-        "org.apache.spark.sql.cassandra"
-    ).options(
-        table="basic_position",
-        keyspace="ais_datas"
-    ).load()
-df.createOrReplaceTempView("vessels")
+
+def load_data(spark, sqlContext):
+    """pre-load data"""
+
+    vessels = sqlContext.read.format(
+            "org.apache.spark.sql.cassandra"
+        ).options(
+            table="basic_position",
+            keyspace="ais_datas"
+        ).load().createOrReplaceTempView("vessels")
+
+    v = spark.sql("SELECT * FROM vessels")
+
+    return v
+
+
+def get_all(spark, sqlContext, before, after):
+    """get any point between two timestamps"""
+
+    v = load_data(spark, sqlContext)
+
+    p = v.filter(
+        v.timestamps >= after
+    ).filter(
+        v.timestamps < before
+    ).collect()
+
+# CLEANING TO FINISH
+
 t = spark.sql("SELECT mmsi, timestamps, latitude, longitude FROM vessels WHERE timestamps >= \"2016-12-10 22:53:00\" AND timestamps < \"2016-12-10 22:54:00\" ")
 t.createOrReplaceTempView("unique_time")
 coo = spark.sql("SELECT latitude, longitude FROM unique_time")
--- recurrent_routes.py
+++ recurrent_routes.py
@@ -18,7 +18,7 @@ from sklearn import svm
 
 
 def load_dest_data(spark, sqlContext):
-    """LOAD DATAS"""
+    """initial data loading function"""
 
     # load tables and create a view
     positions = sqlContext.read.format(
@@ -35,6 +35,7 @@ def load_dest_data(spark, sqlContext):
         keyspace="ais_datas"
     ).load().createOrReplaceTempView("dest")
 
+    # not collected yet
     d = spark.sql("SELECT * FROM dest")
     p = spark.sql("SELECT * FROM pos")
 
@@ -42,12 +43,15 @@ def load_dest_data(spark, sqlContext):
 
 
 def get_all_to_dest(spark, sqlContext, destination):
-    """PLOTS EVERY POINT GOING TO A DEST"""
+    """makes the link between destination and position tables"""
 
     d, p = load_dest_data(spark, sqlContext)
 
+    # select every mmsi - day couple going to the destination
     dest = d.filter(d.destination == destination).collect()
     rows = list()
+
+    # for each mmsi-day, get the positions of that mmsi that day
     for row in dest:
         mmsi = row['mmsi']
         date_to = row['timestamps'].date()
@@ -65,16 +69,26 @@ def get_all_to_dest(spark, sqlContext, destination):
 
 def plot_all_to_dest(spark, sqlContext, destination, filename="plot",
                      file_path="./plots/"):
+    """Use SVM to approximate a density map of the previously retrieved
+    vessels, going to the specified destination"""
+
     filename += '_'
     rows = get_all_to_dest(spark, sqlContext, destination)
 
     # SVM
+    # Format data the way we need
     data = np.array(
         [[row['longitude'], row['latitude']] for row in rows]
         )
+
+    # Apply SVM
     OCS = svm.OneClassSVM(nu=0.1, kernel="rbf", gamma=1)
     OCS.fit(data)
+
+    # divide 2D space into subdivisions
     xx, yy = np.meshgrid(np.linspace(-4, 37, 50), np.linspace(26, 48, 50))
+
+    # get the decision boundary on that space subdivision
     Z = OCS.decision_function(np.c_[xx.ravel(), yy.ravel()])
     Z = Z.reshape(xx.shape)
 
@@ -91,11 +105,11 @@ def plot_all_to_dest(spark, sqlContext, destination, filename="plot",
     e = m.drawcoastlines()
     e = m.drawcountries()
     e = m.drawmapboundary(fill_color='steelblue')
-    x, y = m(xx, yy)
+    x, y = m(xx, yy)  # mandatory
     e = m.contourf(x, y, Z, levels=np.linspace(Z.min(), 0, 7), cmap=plt.cm.PuBu)
     e = m.contour(x, y, Z, levels=[0], linewidths=2, colors='darkred')
     e = m.contourf(x, y, Z, levels=[0, Z.max()], colors='palevioletred')
-    x, y = m(data[:, 0], data[:, 1])
+    x, y = m(data[:, 0], data[:, 1])  # mandatory
     e = m.scatter(x, y, c='gold', s=10)
     e = m.fillcontinents(color='gainsboro', lake_color="lime")
     # plt.show()
